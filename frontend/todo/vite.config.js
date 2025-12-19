106c94b0d9c06367e2111af6869820072b0db4dd
ayush-khobragade9921 <ayushkhobmicro@outlook.com>
2025-12-20 02:44:02 +0530

    remove tailwind vite plugin for vercel deploy

--- frontend/todo/vite.config.js
+++ frontend/todo/vite.config.js
@@ -1,17 +1,6 @@
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-
-  server: {
-    port: 5173, // optional, default port
-    proxy: {
-      "/api": {
-        target: "http://localhost:5000",
-        changeOrigin: true,
-      },
-    },
-  },
-});
+  plugins: [react()],
+})
